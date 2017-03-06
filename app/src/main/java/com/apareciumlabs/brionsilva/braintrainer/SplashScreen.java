8c8c97346ca92e7c9543bf720c9593f5efab1af0
Brion Mario <brion.2015283@iit.ac.lk>
2017-03-06 21:17:56 +0530

    Added the menu screen

--- app/src/main/java/com/apareciumlabs/brionsilva/braintrainer/SplashScreen.java
+++ app/src/main/java/com/apareciumlabs/brionsilva/braintrainer/SplashScreen.java
@@ -1,13 +1,45 @@
 package com.apareciumlabs.brionsilva.braintrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
+    private final int DURATION =5000;
+    private Thread mSplashThread;
+    private ProgressBar progressBar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+        mSplashThread = new Thread() {
+
+            @Override
+            public void run() {
+                synchronized (this) {
+                    try {
+                        wait(1000);
+                        progressBar.setProgress(25);
+                        wait(2000);
+                        progressBar.setProgress(50);
+                        wait(1500);
+                        progressBar.setProgress(80);
+                        wait(DURATION);
+                    } catch (InterruptedException e) {
+                    } finally {
+                        progressBar.setProgress(100);
+                        finish();
+                        Intent intent = new Intent(getBaseContext(),
+                                MainMenu.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+
+        };
+        mSplashThread.start();
     }
 }
